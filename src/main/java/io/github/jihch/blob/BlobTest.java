b64604b20d94c6edb2ac63c1c40ba4f6476579ed
jihch <haoqibaobaoji@126.com>
2023-01-02 11:29:47 +0800

    Update BlobTest.java

--- src/main/java/io/github/jihch/blob/BlobTest.java
+++ src/main/java/io/github/jihch/blob/BlobTest.java
@@ -1,13 +1,11 @@
 package io.github.jihch.blob;
 
+import io.github.jihch.bean.Customer;
 import io.github.jihch.util.JDBCUtils;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.io.*;
+import java.sql.*;
 
 /**
  * 测试使用 PreparedStatement 操作 Blob 类型的数据
@@ -27,6 +25,74 @@ public class BlobTest {
         preparedStatement.setBlob(4, inputStream);
         preparedStatement.execute();
         JDBCUtils.closeResource(connection, preparedStatement);
+    }
+
+    //查询数据表 customers 中 blob 类型的字段
+    @Test
+    public void testQuery() {
+        Connection connection = null;
+        String sql = "select id, name, email, birth, photo from customers where id = ?";
+        PreparedStatement preparedStatement = null;
+        InputStream binaryStream = null;
+        FileOutputStream fileOutputStream = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = JDBCUtils.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, 5);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+//            方式一：
+//            int id = resultSet.getInt(1);
+//            String name = resultSet.getString(2);
+//            String email = resultSet.getString(3);
+//            Date birth = resultSet.getDate(4);
+
+                //方式二：
+                //一般来说方式二优于方式一，因为变更sql中查询字段顺序的时候不会影响到已经存在取结果集字段的代码
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                Date birth = resultSet.getDate("birth");
+
+                Customer customer = new Customer(id, name, email, birth);
+                System.out.println(customer);
+
+                //将 Blob 类型的字段下载下来，以文件的方式保存在本地
+                Blob photo = resultSet.getBlob("photo");
+                binaryStream = photo.getBinaryStream();
+                fileOutputStream = new FileOutputStream(".\\zhangsanfeng_query.jpg");
+                byte[] buffer = new byte[1024];
+                int len;
+                while ((len = binaryStream.read(buffer)) != -1) {
+                    fileOutputStream.write(buffer, 0, len);
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (binaryStream != null) {
+                try {
+                    binaryStream.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            JDBCUtils.closeResource(connection, preparedStatement, resultSet);
+        }
+
     }
 
 }
